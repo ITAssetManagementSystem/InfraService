863350a822545650e83f095d47b311be762c58d0
Nilay Saxena <nilaysaxena94@gmail.com>
2026-01-01 12:24:48 +0000

    refactor: uncomment Azure Container Registry, AKS, and Key Vault modules in main.tf for resource provisioning

--- environment/dev/main.tf
+++ environment/dev/main.tf
@@ -29,120 +29,120 @@ module "subnet" {
   subnet     = var.subnet
 }
 
-# ############################
-# # Azure Container Registry
-# ############################
-# module "acr" {
-#   depends_on = [module.rg]
-#   source     = "../../module/azure_container_registry"
-#   acr        = var.acr
-# }
+############################
+# Azure Container Registry
+############################
+module "acr" {
+  depends_on = [module.rg]
+  source     = "../../module/azure_container_registry"
+  acr        = var.acr
+}
 
-# ############################
-# # AKS Cluster
-# ############################
-# module "aks" {
-#   depends_on = [module.rg]
-#   source     = "../../module/azure_kubernetes"
-#   aks        = var.aks
-# }
+############################
+# AKS Cluster
+############################
+module "aks" {
+  depends_on = [module.rg]
+  source     = "../../module/azure_kubernetes"
+  aks        = var.aks
+}
 
-# ############################
-# # AKS ↔ ACR Role Assignment
-# ############################
-# module "aks_acr_role" {
-#   depends_on = [module.aks, module.acr]
-#   source     = "../../module/aks_acr_role_assignment"
+############################
+# AKS ↔ ACR Role Assignment
+############################
+module "aks_acr_role" {
+  depends_on = [module.aks, module.acr]
+  source     = "../../module/aks_acr_role_assignment"
 
-#   assignments = {
-#     for k, v in var.assignments : k => {
-#       acr_id            = module.acr.acr_ids[v.acr_key]
-#       kubelet_object_id = module.aks.aks_kubelet_ids[v.aks_key]
-#     }
-#   }
-# }
+  assignments = {
+    for k, v in var.assignments : k => {
+      acr_id            = module.acr.acr_ids[v.acr_key]
+      kubelet_object_id = module.aks.aks_kubelet_ids[v.aks_key]
+    }
+  }
+}
 
-# # resource "time_sleep" "wait_for_kv_rbac" {
-# #   depends_on = [
-# #     module.keyvault_rbac
-# #   ]
-
-# #   create_duration = "120s"
-# # }
-
-
-# ############################
-# # Key Vault + Secret + RBAC
-# ############################
-# module "keyvault" {
+# resource "time_sleep" "wait_for_kv_rbac" {
 #   depends_on = [
-#     module.rg, module.aks
-#     # time_sleep.wait_for_kv_rbac
+#     module.keyvault_rbac
 #   ]
 
-#   source = "../../module/key_vault"
-#   kv     = var.kv
-#   sec    = var.sec
-
-#   keyvault_assignments = {
-#     for k, v in var.keyvault_assignments : k => {
-#       scope = module.keyvault.key_vault_ids[v.kv_key]
-#       principal_id = (
-#         v.principal_type == "terraform"
-#         ? data.azurerm_client_config.current.object_id
-#         : module.aks.aks_kubelet_ids["aks1"]
-#       )
-
-#       role_name = (
-#         v.principal_type == "terraform"
-#         ? "Key Vault Secrets Officer"
-#         : "Key Vault Secrets User"
-#       )
-#       kv_key = v.kv_key
-#     }
-#   }
-
+#   create_duration = "120s"
 # }
 
 
+############################
+# Key Vault + Secret + RBAC
+############################
+module "keyvault" {
+  depends_on = [
+    module.rg, module.aks
+    # time_sleep.wait_for_kv_rbac
+  ]
+
+  source = "../../module/key_vault"
+  kv     = var.kv
+  sec    = var.sec
+
+  keyvault_assignments = {
+    for k, v in var.keyvault_assignments : k => {
+      scope = module.keyvault.key_vault_ids[v.kv_key]
+      principal_id = (
+        v.principal_type == "terraform"
+        ? data.azurerm_client_config.current.object_id
+        : module.aks.aks_kubelet_ids["aks1"]
+      )
+
+      role_name = (
+        v.principal_type == "terraform"
+        ? "Key Vault Secrets Officer"
+        : "Key Vault Secrets User"
+      )
+      kv_key = v.kv_key
+    }
+  }
+
+}
 
 
-# ############################
-# # PostgreSQL Server
-# ############################
-# module "postgresql" {
-#   depends_on = [
-#     module.rg,
-#     module.keyvault
-#   ]
 
-#   source = "../../module/postgre_sql"
 
-#   postgres_sql  = var.postgres_sql
-#   key_vault_ids = module.keyvault.key_vault_ids
-# }
+############################
+# PostgreSQL Server
+############################
+module "postgresql" {
+  depends_on = [
+    module.rg,
+    module.keyvault
+  ]
 
-# ############################
-# # PostgreSQL Database
-# ############################
+  source = "../../module/postgre_sql"
 
-# module "postgredb" {
-#   depends_on          = [module.postgresql]
-#   source              = "../../module/postgre_db"
-#   postgres_db         = var.postgres_db
-#   postgres_server_ids = module.postgresql.postgres_server_ids
-# }
+  postgres_sql  = var.postgres_sql
+  key_vault_ids = module.keyvault.key_vault_ids
+}
 
-# ############################
-# # PostgreSQL Firewall Rules
-# ############################
-# module "postgres_firewall" {
-#   depends_on = [
-#     module.postgresql
-#   ]
+############################
+# PostgreSQL Database
+############################
 
-#   source = "../../module/postgre_firewall"
+module "postgredb" {
+  depends_on          = [module.postgresql]
+  source              = "../../module/postgre_db"
+  postgres_db         = var.postgres_db
+  postgres_server_ids = module.postgresql.postgres_server_ids
+}
 
-#   firewall_rules      = var.postgres_firewall_rules
-#   postgres_server_ids = module.postgresql.postgres_server_ids
-# }
+############################
+# PostgreSQL Firewall Rules
+############################
+module "postgres_firewall" {
+  depends_on = [
+    module.postgresql
+  ]
+
+  source = "../../module/postgre_firewall"
+
+  firewall_rules      = var.postgres_firewall_rules
+  postgres_server_ids = module.postgresql.postgres_server_ids
+}
